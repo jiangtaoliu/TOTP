cd62222abfd97b6ed489b93bb3b74ee459719d2a
Jett <jet.lau@gmail.com>
2016-12-23 18:00:29 +1300

    Update testotp.py

--- testotp.py
+++ testotp.py
@@ -1,6 +1,5 @@
 import pyotp
-str = "jet.lau@gmail.comHDECHALLENGE003"
-#str = "JSXILTMMF2UAZ3NMFUWYLTDN5WUQRCFINEECTCMIVHEORJQGAZQ===="
+str = "testpyotp"
 #str = str.replace('\x00', '')
 #str = str.encode('latin-1').decode('utf-16le')
 print(str)
